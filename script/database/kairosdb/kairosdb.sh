5b1b1eea82e0c4ea678ac4fc31723074aae00619
at15 <at15@outlook.com>
2018-03-02 00:48:30 -0800

    [db] Failed to use new waitforit no such file
    - it does not look a docker cache problem ...
    - https://github.com/benchhub/benchhub/issues/20

--- script/database/kairosdb/kairosdb.sh
+++ script/database/kairosdb/kairosdb.sh
@@ -2,6 +2,8 @@
 
 echo "waiting for cassandra to start"
 # TODO： timeout is not included
-wait-for-it cassandra:9042
+#wait-for-it cassandra:9042
+ls
+/usr/bin/waitforit -w tcp://cassandra:9042
 echo "cassandra started"
 /opt/kairosdb/bin/kairosdb.sh run
